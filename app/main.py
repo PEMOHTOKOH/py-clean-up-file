d1306104ef29eef84600f20ea2705311bf072f59
Andrey <90110501+PEMOHTOKOH@users.noreply.github.com>
2025-04-01 20:40:08 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,9 +6,7 @@ class CleanUpFile:
         self.filename = filename
 
     def __enter__(self) -> "CleanUpFile":
-        self.file = open(self.filename, "w")
         return self
 
     def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
-        self.file.close()
         os.remove(self.filename)
